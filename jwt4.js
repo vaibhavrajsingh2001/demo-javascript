9e9f3d51557ed3c1bdfc4238de114ea862fe6343
Anto Christopher <anto@deepsource.io>
2022-02-28 16:30:44 +0530

    jwt positive

--- jwt4.js
+++ jwt4.js
@@ -1,3 +1,3 @@
 import { sign as jsonsign } from "jsonwebtoken";
 
-const token = jsonsign({ user: "xyz" }, key, { algorithm: "none" });
+const token = jsonsign({ user: 'xyz' }, key, { algorithm: 'HS256' });
