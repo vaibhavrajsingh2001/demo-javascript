b31555604d9d2af80026fd47a49de37028aae9ba
Anto Christopher <anto@deepsource.io>
2022-06-06 10:45:54 +0530

    Type checking test

--- packages/demo-typescript/src/utils.ts
+++ packages/demo-typescript/src/utils.ts
@@ -19,3 +19,37 @@ export const roles: Array<any> = ['admin', 'manager'];
 export class MockUser {
   constructor(readonly name: string) {}
 }
+
+type A = 1 | 2 | 3 | "3"
+
+type customtype = number
+let a: string | customtype
+let b: string & customtype
+
+type A1 = 1 | 2 | 3 | 1
+type B = "foo" | "bar" | "foo"
+type C = A1 | B | A1 | B
+
+let a1: number | number | string
+
+function foobar(a: number | number): string | string { return 'a'}
+
+type customtype11 = number
+const a11: customtype11 | string | customtype11 | string = 'a'
+
+export interface Foo {
+  bar: string;
+  quz?: string | undefined;
+  baz?: string | undefined | number;
+}
+
+interface Foo2 {
+  bar: string;
+  quz?: string;
+}
+
+interface Foo1 {
+  bar: string;
+  quz?: string | undefined;
+  baz?: string | undefined | number;
+}
